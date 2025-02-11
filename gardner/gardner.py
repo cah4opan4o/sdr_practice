385c6d2190fbca81785f0e5f20a18617584a7bac
cah4opan4o <volodin.sasha2004@yandex.ru>
2025-02-11 18:28:58 +0700

    Gardner worked with data/txdata7.pcm

--- gardner/gardner.py
+++ gardner/gardner.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import firwin, lfilter
+# from scipy.signal import firwin, lfilter
 
 def read_file(file_path):
     dtype = np.int16
@@ -31,11 +31,11 @@ def read_file(file_path):
 #     return new_offset
 
 
-def gardner_timing_recovery(I, Q, Nsps, BnTs=0.01, zeta=1.0, Kp=1.0):
+def gardner_timing_recovery(I, Q, Nsps, BnTs=0.01, damp=1.0, Kp=1.0):
     # Вычисление параметров K1, K2
-    theta = (BnTs * Nsps) / (zeta + 1 / (4 * zeta))
-    K1 = (-4 * theta**2) / ((1 + 2 * zeta * theta + theta**2) * Kp)
-    K2 = (-4 * theta**2) / ((1 + 2 * zeta * theta + theta**2) * Kp)
+    theta = (BnTs / Nsps) / (damp + 1 / (4 * damp))
+    K1 = (-4 * theta * damp) / ((1 + 2 * damp * theta + theta**2) * Kp)
+    K2 = (-4 * theta**2) / ((1 + 2 * damp * theta + theta**2) * Kp)
     
     # Начальная инициализация переменных
     p1 = 0
@@ -43,19 +43,21 @@ def gardner_timing_recovery(I, Q, Nsps, BnTs=0.01, zeta=1.0, Kp=1.0):
     offset = 0  # Начальное смещение
     recovered_I = []
     recovered_Q = []
+    ted = []
+    offset_array = []
     
-    for n in range(len(I) - 2 * Nsps):
-        e = (I[n + Nsps - Nsps] - I[n + Nsps]) * \
-            (Q[n + Nsps // 2 + Nsps] - Q[n + Nsps // 2 - Nsps])
+    for n in range(1,len(I) - 2 * Nsps,Nsps):
+        e = (I[n + Nsps + offset] - I[n + offset]) * I[n + Nsps//2 + offset] +\
+              (Q[n + Nsps + offset] - Q[n + offset]) * Q[n + Nsps//2 + offset]
         
         # Обновление p1, p2
         p1 = e * K1
         p2 += p1 + e * K2
         
         # Коррекция задержки
-        if p2 >= 1:
+        while (p2 > 1):
             p2 -= 1
-        elif p2 <= -1:
+        while (p2 < -1):
             p2 += 1
         
         # Определение текущего индекса выборки
@@ -66,60 +68,73 @@ def gardner_timing_recovery(I, Q, Nsps, BnTs=0.01, zeta=1.0, Kp=1.0):
         if 0 <= sample_index < len(I):
             recovered_I.append(I[sample_index])
             recovered_Q.append(Q[sample_index])
+            ted.append(e)
+            offset_array.append(offset)
     
-    return np.array(recovered_I), np.array(recovered_Q)
+    return np.array(recovered_I), np.array(recovered_Q), np.array(ted), np.array(offset_array)
 
 
 
 if __name__ == "__main__":
-    file_path = "D:\\Github\\sdr_practice\\gardner\\data\\txdata.pcm"
+    file_path = "/home/plutosdr/Desktop/ia232/sdr_practice/gardner/data/txdata7.pcm"
     Nsp = 10 # sample per symbol
-    Ns = 0 # shift
-    offset = 0
-    step_offset = 0.01
-
 
     I,Q = read_file(file_path)
-
 
     # Приведение к -1 и 1, max=2**11
     I = I / max(abs(I))
     Q = Q / max(abs(Q))
 
     plt.figure(figsize=(16,8))
-    plt.subplot(3,2,1)
+    plt.subplot(4,2,1)
     plt.title("raw data")
     plt.plot(Q,label="Q")
     plt.plot(I,label="I")
-    plt.xlabel("Time")
-    plt.ylabel("Amplitude")
+    # plt.xlabel("Time")
+    # plt.ylabel("Amplitude")
     plt.legend()
+
+    plt.subplot(4,2,2)
+    plt.title("raw data")
+    plt.scatter(Q,I)
     
     filter = np.ones(10)
     I = np.convolve(I,filter,mode='valid')
     Q = np.convolve(Q,filter,mode='valid')
     
-    plt.subplot(3,2,2)
+    plt.subplot(4,2,3)
     plt.title("after filter")
     plt.plot(Q,label="Q")
     plt.plot(I,label="I")
-    plt.xlabel("Time")
-    plt.ylabel("Amplitude")
+    # plt.xlabel("Time")
+    # plt.ylabel("Amplitude")
     plt.legend()
     
     # QPSK before Gardner 
-    plt.subplot(3,2,3)
+    plt.subplot(4,2,4)
     plt.scatter(Q,I)
     plt.title("QPSK before Gardner")
 
     # /////////////////////////////////////////////////////////////////////////
-    output_I, output_Q = gardner_timing_recovery(I, Q, Nsp) # Nsp = Nsps
+    output_I, output_Q, ted, offset = gardner_timing_recovery(I, Q, Nsp) # Nsp = Nsps
     # /////////////////////////////////////////////////////////////////////////
     
+    plt.subplot(4,2,5)
+    plt.plot(ted)
+    plt.title("TED")
+    
     # QPSK after Gardner
-    plt.subplot(3,2,4)
+    plt.subplot(4,2,6)
     plt.scatter(output_Q, output_I)
     plt.title("QPSK after Gardner")
     
+    plt.subplot(4,2,7)
+    plt.plot(offset)
+    plt.title("offset")
+
+    # plt.subplot(4,2,8)
+    # plt.plot(offset,ted)
+    # plt.title("Waveform")
+
     plt.grid()
     plt.show()
